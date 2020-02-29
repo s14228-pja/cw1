6cce74637c07c5bc57ef6d4185b879f3110854a7
s14228 <s14228@pjwstk.edu.pl>
2020-02-29 21:21:45 +0100

    get http

--- Cwiczenie1/Cwiczenie1/Program.cs
+++ Cwiczenie1/Cwiczenie1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenie1
 {
     public class Program
     {
-        public static void Main(string[] args)
-        { 
-            Console.WriteLine("Hello World!");
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[z-z0-9]*@[a-z0-9]+\\.[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+
+            Console.WriteLine("Koniec!");
         }
     }
 }

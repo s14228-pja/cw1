459786e8ceb2ebe095d5bb6709cd0f1b3bb76246
s14228 <s14228@pjwstk.edu.pl>
2020-02-29 21:00:45 +0100

    dodanie spacji

--- Cwiczenie1/Cwiczenie1/Program.cs
+++ Cwiczenie1/Cwiczenie1/Program.cs
@@ -5,7 +5,7 @@ namespace Cwiczenie1
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Hello World!");
         }
     }

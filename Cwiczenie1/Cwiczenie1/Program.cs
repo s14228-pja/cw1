cec2403d06791ff5ac6419d2772b73bacab746dd
s14228 <s14228@pjwstk.edu.pl>
2020-02-29 20:57:49 +0100

    Dodałem projekt cwiczenie1

---
+++ Cwiczenie1/Cwiczenie1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwiczenie1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
